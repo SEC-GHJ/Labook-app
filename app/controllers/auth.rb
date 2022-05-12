6fd8c175c33f4bccc4d0c9775b864e48886f9f2a
June Hsu <junehsu920179@gmail.com>
2022-05-12 23:02:12 +0800

    add auth route

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -24,19 +24,45 @@ module Labook
           SecureSession.new(session).set(:current_account, account)
           flash[:notice] = "Welcome back #{account['account']}!"
           routing.redirect '/'
-        rescue StandardError => e
+        rescue AuthenticateAccount::UnauthorizedError
           flash.now[:error] = 'Account and password did not match our records'
-          puts e.message
           response.status = 400
           view :login
+        rescue AuthenticateAccount::ApiServerError => e
+          App.logger.warn "API server error: #{e.inspect}\n#{e.backtrace}"
+          flash[:error] = 'Our servers are not responding -- please try later'
+          response.status = 500
+          routing.redirect @login_route
         end
       end
 
+      @logout_route = '/auth/logout'
       routing.on 'logout' do
         routing.get do
           SecureSession.new(session).delete(:current_account)
+          flash[:notice] = "You've been logged out"
           session[:current_account] = nil
           routing.redirect @login_route
+        end
+      end
+
+      @register_route = '/auth/register'
+      routing.is 'register' do
+        routing.get do
+          view :register
+        end
+
+        routing.post do
+          account_data = JsonRequestBody.symbolize(routing.params)
+          CreateAccount.new(App.config).call(**account_data)
+
+          flash[:notice] = 'Please login with your new account information'
+          routing.redirect @login_route
+        rescue StandardError => e
+          App.logger.error "ERROR CREATING ACCOUNT: #{e.inspect}"
+          App.logger.error e.backtrace
+          flash[:error] = 'Could not create account'
+          routing.redirect @register_route
         end
       end
     end
