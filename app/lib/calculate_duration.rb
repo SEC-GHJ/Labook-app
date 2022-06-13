a3425621d1ee0852d5dc907f321b64ffc5c51673
ginagig123 <ginagigo123@gmail.com>
2022-06-13 22:12:41 +0800

    chat sort finish

---
+++ app/lib/calculate_duration.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'time_difference'
+
+# Calculate the last seen duration
+class CalculateDuration
+  def self.format(unit, value)
+    "#{value} #{unit.to_s.singularize.pluralize(value)}"
+  end
+
+  def self.duration(time)
+    created_at = Time.parse(time)
+    duration = TimeDifference.between(created_at, Time.now).in_general
+
+    # return value once value is not 0
+    duration.each do |unit, value|
+      return format(unit, value) if value != 0
+    end
+  end
+end
--- app/models/messages.rb
+++ app/models/messages.rb
@@ -1,28 +1,20 @@
 # frozen_string_literal: true
 
 require_relative 'message'
-require 'time_difference'
+require_relative '../lib/calculate_duration'
 
 module Labook
   # Behaviors of the all the chatrooms
   class Messages
     attr_reader :all, :last_seen
 
-    def duration(time)
-      created_at = Time.parse(time)
-      duration = TimeDifference.between(created_at, Time.now).in_general
-
-      duration.each do |unit, value|
-        return "#{value} #{unit.to_s.singularize.pluralize(value)}" if value != 0
-      end
-    end
-
     def initialize(messages_list)
       @all = messages_list.map do |message|
         Message.new(message)
       end
 
-      @last_seen = all.last.nil? ? nil : duration(all.last.created_at)
+      # @last_seen = all.last.nil? ? nil : CalculateDuration.duration(all.last.created_at)
+      @last_seen = CalculateDuration.duration(all.last.created_at)
     end
   end
 end
